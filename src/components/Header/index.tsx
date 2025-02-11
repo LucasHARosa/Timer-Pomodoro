52d6f25d63f3b42e320806d457e85b08706891a2
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2025-02-11 19:45:58 +0000

    Fix homepage URL and update navigation links to use hyphenated path

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,10 +11,10 @@ export function Header() {
         <img src={logoIgnite} alt="" />
       </span>
       <nav>
-        <NavLink to="/Timer_Pomodoro/" title="Timer">
+        <NavLink to="/Timer-Pomodoro/" title="Timer">
           <Timer size={24} />
         </NavLink>
-        <NavLink to="/Timer_Pomodoro/history/" title="Histórico">
+        <NavLink to="/Timer-Pomodoro/history/" title="Histórico">
           <Scroll size={24} />
         </NavLink>
       </nav>
